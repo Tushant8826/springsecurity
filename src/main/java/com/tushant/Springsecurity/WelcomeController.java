faacfe3a234b25563a601ef83a77705cc581d68c
Tushant <tushant@Tushants-MacBook-Air.local>
2024-05-13 21:02:45 +0530

    create spring boot app withoyt spring security

---
+++ src/main/java/com/tushant/Springsecurity/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.tushant.Springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+       return "Welcome to Spring Application with out security";
+    }
+}
